e71858cb7999e3750ea47023302130aa253c6395
Muhammad Ranju <mdranju23@gmail.com>
2024-10-28 21:29:39 +0600

    first commit

---
+++ day_1/src/main.jsx
@@ -0,0 +1,27 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/about",
+    element: <div>This about page</div>,
+  },
+  {
+    path: "/contact",
+    element: <div>This contact page</div>,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    {/* <App /> */}
+    <RouterProvider router={router} />
+  </StrictMode>
+);
